ca7aa2cb1c4bb675d9e79a3d21509b4657710f2d
Brian Illes <brianrilles@gmail.com>
2019-08-22 08:36:15 -0700

    Feeds scaffold

---
+++ api/feeds/feedServices.js
@@ -0,0 +1,17 @@
+module.exports = {
+  completedPollsFeed,
+  activePollsFeed,
+  proposedPollsFeed
+};
+
+function completedPollsFeed(reqData) {
+  return reqData;
+}
+
+function activePollsFeed(reqData) {
+  return reqData;
+}
+
+function proposedPollsFeed(reqData) {
+  return reqData;
+}
